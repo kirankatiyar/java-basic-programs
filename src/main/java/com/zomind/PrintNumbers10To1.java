87fc1977d49cbe981fc07bb8b56a85798b9374d4
Kiran Katiyar <kirankatiyar16@gmail.com>
2026-04-17 16:28:28 +0530

    Print numbers from 10 to 1 program

---
+++ src/main/java/com/zomind/PrintNumbers10To1.java
@@ -0,0 +1,10 @@
+package com.zomind;
+
+public class PrintNumbers10To1 {
+    public static void main(String[] args){
+        System.out.println("Numbers from 10 to 1");
+        for(int i=10;i>=1;i--){
+            System.out.print(i+",");
+        }
+    }
+}
